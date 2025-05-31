a570d95f1cdfbcc320ddad0c343cd9aae56bb102
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2025-05-31 19:59:31 +0100

    chore and feat: Instalacoes de bibliotecas como shadcn e lucid react, inicio da criacao da pagina inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { ArrowRight, HandCoins } from "lucide-react";
+
 export default function Home() {
   return (
-    <main className="bg-amber-50 min-h-screen flex items-center justify-center">
-      <div className="bg-clip-text text-transparent bg-gradient-to-r from-amber-600 to-amber-400 text-4xl">
-        Hello world!
-      </div>
-    </main>
+    <div className="h-screen bg-gradient-to-b from-white to-amber-50">
+      <header className="h-16">
+        <HandCoins />
+        <span>ApoiaDev</span>
+      </header>
+      <main>
+        <section>
+          <p>Plataforma criada para conteúdo</p>
+          <h2>Monetize seu publico de forma descomplicada</h2>
+          <p>
+            Receba doações directamente de seus seguidores através de uma pagina
+            personalizada e elegante, sem complicações
+          </p>
+          <Button>
+            Começar agora
+            <ArrowRight className="w-4 h-4" />
+          </Button>
+        </section>
+      </main>
+    </div>
   );
 }
