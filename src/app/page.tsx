336d3b332a7cc7f7a4a0ac34085c018fcf019af9
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2025-05-31 13:24:30 +0100

    Chore: Configuração inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
+    <main className="bg-amber-50 min-h-screen flex items-center justify-center">
+      <div className="bg-clip-text text-transparent bg-gradient-to-r from-amber-600 to-amber-400 text-4xl">
+        Hello world!
+      </div>
     </main>
   );
 }
